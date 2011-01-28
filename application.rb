b12dbebe40b5d94515a22b78a90fd5e697dd3b54
Valery Vishnyakov <balepc@gmail.com>
2011-01-28 13:40:17 +0200

    Remove ip hook

--- application.rb
+++ application.rb
@@ -15,8 +15,8 @@ helpers do
 end
 
 get '/' do
-  ip_address = '83.99.239.4'
-#  ip_address = @env['REMOTE_ADDR']
+#  ip_address = '83.99.239.4'
+  ip_address = @env['REMOTE_ADDR']
 
   @ip1, @time1 = IpGroupCity.find_by_ip(ip_address)
   @ip2, @time2 = MaxMind.find_by_ip(ip_address)
